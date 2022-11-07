eb3f7cc85648a2c5c89c20f743ab0bc7db242915
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-07 05:23:00 +0000

    [add]3-2ストロングパラメーター

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,11 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
   def index
   end
 
@@ -11,4 +16,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
